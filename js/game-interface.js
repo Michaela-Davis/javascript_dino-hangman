97c29ab8b6d50a48317eb55df3d9046622477ff3
Michaela Davis and Patrick McGreevy <patrick@>
2017-03-15 11:00:04 -0700

    Implement guess submission

--- js/game-interface.js
+++ js/game-interface.js
@@ -2,21 +2,74 @@
 
 var Game = require('./../js/game.js').gameModule;
 
+var alphabet = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+var currentGameObject;
+
 $(document).ready(function() {
   $('form#new-game').submit(function(event) {
     event.preventDefault();
-    var currentGameObject = new Game();
+    currentGameObject = new Game();
     currentGameObject.setupWord();
-    setupGameDisplay(currentGameObject);
+    updateGameDisplay(currentGameObject);
     $('#game-display').show();
+  });
+  $('form#guess-form').submit(function(event) {
+    event.preventDefault();
+    var guess = $('#guess').val().slice(0, 1);
+    makeGuess(currentGameObject, guess);
   });
 });
 
-function setupGameDisplay(game) {
+function updateGameDisplay(game) {
   var letters = game.currentWord.length;
   var blanks = '';
   for (var i = 0; i < letters; i++) {
     blanks += '_ ';
   }
   $('p#blanks').text(blanks);
+
+  var alphabetDisplay = '';
+  for (var i = 0; i < alphabet.length; i++) {
+    var letter = alphabet[i];
+    alphabetDisplay += (game.guessedLetters.indexOf(letter) == -1 ?
+      letter : '_') + ' ';
+  }
+  $('p#letters-guessed').text(alphabetDisplay);
+}
+
+function makeGuess(game, letter) {
+  console.log('makeGuess()');
+  if (game.makeGuess(letter))
+    updateGameDisplay(game);
+  else {
+    alert('You have already guessed that letter!');
+  }
 }
--- js/game.js
+++ js/game.js
@@ -9,6 +9,14 @@ Game.prototype.setupWord = function() {
   this.currentWord = testWord;
 };
 
+Game.prototype.makeGuess = function(letter) {
+  console.log('Game.prototype.makeGuess()');
+  if (this.guessedLetters.indexOf(letter) != -1)
+    return false;
+  this.guessedLetters.push(letter.toLowerCase());
+
+  return true;
+};
 
 
 exports.gameModule = Game;
